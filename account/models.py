143c33c2a2e23efbdeafd0c7695e3b901e6bb240
eldos <eldos.baktybekov@gmail.com>
2022-09-05 18:53:59 +0600

    :card_file_box: Add models and custom user

---
+++ account/models.py
@@ -0,0 +1,78 @@
+from uuid import uuid4
+
+from django.contrib.auth import models as auth_models
+from django.db import models
+from django.utils.crypto import get_random_string
+from django.utils.translation import gettext_lazy
+
+
+class UserManager(auth_models.BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Users require an email field')
+        username = get_random_string(32)
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(auth_models.AbstractUser):
+    email = models.EmailField(gettext_lazy('email address'), unique=True, max_length=64)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+class Company(models.Model):
+    uuid = models.UUIDField(default=uuid4, unique=True, editable=False)
+    name = models.CharField(max_length=255, unique=True)
+    merchant_id = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class OffersStore(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    marketplace_store_id = models.CharField(max_length=255)
+
+
+class Offer(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    sku = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255, null=True, blank=True)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class OfferAvailability(models.Model):
+    product = models.ForeignKey(Offer, on_delete=models.CASCADE)
+    store = models.ForeignKey(OffersStore, on_delete=models.CASCADE)
+    is_available = models.BooleanField()
