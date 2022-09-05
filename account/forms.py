829be5de2527f8e93c64ad8f81aff97e9095dd67
eldos <eldos.baktybekov@gmail.com>
2022-09-05 19:24:29 +0600

    :construction: Change default user in forms

---
+++ account/forms.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import forms as auth_forms
+from django import forms
+
+from .models import User, Company, Offer
+
+
+class UserCreationForm(auth_forms.UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('email',)
+
+
+class UserChangeForm(auth_forms.UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('email',)
+
+
+class CreateCompanyForm(forms.ModelForm):
+    class Meta:
+        model = Company
+        fields = ('name', 'merchant_id')
+
+
+class CreateOfferForm(forms.ModelForm):
+    class Meta:
+        model = Offer
+        fields = ('sku', 'name', 'brand', 'price')
